07f0d5c17bc5d3ab72222831927d79fb3ea371ab
Aravinth <anthu1510@gmail.com>
2024-03-06 21:03:42 +0530

    Added frontend axios integrated

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from "cors"
 import { routes } from '../routes';
 import { errorHandler } from '../middlewares/error.middleware';
 
 const app = express();
 
 // middlewares
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: '5mb' }));
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,48 @@
 
+import axios from "./api/axios"
 import { InfoCircleOutlined } from '@ant-design/icons';
-import { Col, Row, Flex, Button, Form, Input} from 'antd';
+import { Col, Row, Card, Button, Form, Input} from 'antd';
 
 function App() {
 
   const [form] = Form.useForm();
 
+  const handleLoginSubmit = async () => {
+    try {
+      const data = {
+        email: form.getFieldValue('email'),
+        password: form.getFieldValue('password')
+      }
+      const isLoggedIn = await axios.post('/users/login', data);
+      console.log('success', isLoggedIn.data)
+    } catch (error) {
+      console.log('error', error)
+    }
+};
+
   return (
     <Row justify='center' align="middle">
       <Col span={6}>
-        <Form
-          form={form}
-          layout="vertical"
-        >
-          <Form.Item label="Email" required tooltip="This is a required field">
-            <Input placeholder="input placeholder" />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            tooltip={{ title: 'Tooltip with customize icon', icon: <InfoCircleOutlined /> }}
+        <Card title="Card title">
+          <Form
+            form={form}
+            layout="vertical"
+            onFinish={handleLoginSubmit}
           >
-            <Input type='password' placeholder="input placeholder" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary">Submit</Button>
-          </Form.Item>
-        </Form>
+            <Form.Item label="Email" required tooltip="This is a required field">
+              <Input name="email" onChange={(e) => form.setFieldsValue({email: e.target.value})} placeholder="input placeholder" />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              tooltip={{ title: 'Tooltip with customize icon', icon: <InfoCircleOutlined /> }}
+            >
+              <Input type='password' onChange={(e) => form.setFieldsValue({password: e.target.value})} name="password" placeholder="input placeholder" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">Submit</Button>
+            </Form.Item>
+          </Form>
+        </Card>
       </Col>
     </Row>
   )
